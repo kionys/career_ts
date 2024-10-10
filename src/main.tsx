be60caea2045faf86a80b729749f137281201c8a
kionys <kiwon7271@naver.com>
2024-10-10 21:36:40 +0900

    chore: redux provider 주석

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,8 @@ import router from '@/router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 import './index.css';
-import { store } from './store';
 
 const queryClient = new QueryClient();
 
@@ -15,12 +13,13 @@ const rootElement = document.getElementById('root');
 
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        {isDevEnvironment && <ReactQueryDevtools />}
-        <RouterProvider router={router} />
-      </QueryClientProvider>
-    </Provider>
+    // TODO redux 걷어내기
+    // <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      {isDevEnvironment && <ReactQueryDevtools />}
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+    // </Provider>
   );
 } else {
   console.error('Failed to find the root element.');
