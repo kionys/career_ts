c2c575d876f06bc3c6cd52db7eb4787bfb9272e1
kionys <kiwon7271@naver.com>
2024-10-10 21:39:04 +0900

    update: tanstack query 적용을 위한 DTO > currentPage 타입 추가

--- src/api/dtos/productDTO.ts
+++ src/api/dtos/productDTO.ts
@@ -10,6 +10,7 @@ export interface IProduct {
 }
 
 export interface PaginatedProductsDTO {
+  currentPage: number; // TODO 홈에서 상품리스트를 더보기로 패칭하기 위한 페이지 리턴값 추가
   products: IProduct[];
   hasNextPage: boolean;
   totalCount: number;
