acc2ba963331bf6202751851e2d3c62fed23eeeb
kionys <kiwon7271@naver.com>
2024-10-10 21:36:07 +0900

    update: firebase.ts initalize 분기

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,9 @@
-import { initializeApp } from 'firebase/app';
+import { FirebaseApp, getApp, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
+export let app: FirebaseApp;
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -12,8 +13,14 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+try {
+  app = getApp('app');
+} catch (e) {
+  app = initializeApp(firebaseConfig, 'app');
+}
 
+const firebase = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export default firebase;
