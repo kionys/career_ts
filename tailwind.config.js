ab2e57aee3ecd58f65d39d332ba439dcbd092615
kionys <kiwon7271@naver.com>
2024-10-10 21:28:54 +0900

    update: toast 애니메이션 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -66,10 +66,16 @@ module.exports = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        // toast
+        'fade-in': {
+          '0%': { opacity: '0', transform: 'translateY(100%)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'fade-in': 'fade-in 0.5s ease-out forwards', // toast
       },
     },
   },
